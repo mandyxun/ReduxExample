552b5ad03533417c2db0eb6b8a68b00d976cac2e
Mandy Xun <mandyx@Mandys-MacBook-Pro.local>
2017-10-26 11:21:51 -0400

    Redux 1.1

---
+++ ReduxExample/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts' },
+        {title: 'Harry Potter I' },
+        {title: 'The Dark Tower' },
+        {title: 'Eloquent Ruby' },
+        {title: 'Harry Potter II' }
+    ]
+}
