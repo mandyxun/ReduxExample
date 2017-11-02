6b9abea2b72fce3761127f70a54a936de929da8d
Mandy Xun <mandyx@Mandys-MacBook-Pro.local>
2017-11-02 03:15:35 -0400

    reorg

---
+++ Booklist-ReduxExample/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: '我的课程', pages:2 },
+        {title: '预约课程', pages:1 },
+        {title: '已上课程', pages:2 },
+        {title: '我的老师', pages:4 },
+        {title: '阶段测试' , pages:2}
+    ]
+}
