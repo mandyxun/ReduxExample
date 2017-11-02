8d42a302d942309a520ed837b6dec664cdf2065d
Mandy Xun <mandyx@Mandys-MacBook-Pro.local>
2017-11-02 01:45:42 -0400

    middle complete

--- ReduxExample-Intermediate Middleware/src/actions/index.js
+++ ReduxExample-Intermediate Middleware/src/actions/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+
 const API_KEY ='4a5ecb335769214fae47ceeeaea2403c';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
-export function fetchWeather(city,){
+export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);//Ajax request returns a promise 
     //console.log('Request:', request);
--- ReduxExample-Intermediate Middleware/src/containers/weather_list.js
+++ ReduxExample-Intermediate Middleware/src/containers/weather_list.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-//import {selectBook} from '../actions/index';
-//import {bindActionCreators } from 'redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData){
         //console.log(cityData);
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
+        //const lat = cityData.city.coord.lat;
+        //const lon = cityData.city.coord.lon;
+
             return (
                 <tr key={name}>
-                    <td>{name}</td>
+                    <td><GoogleMap lon={lon} lat={lat} /></td>
+                    <td><Chart data={temps} color="orange" units="K"/></td>
+                    <td><Chart data={pressures} color="green" units="hPa"/></td>
+                    <td><Chart data={humidities} color="black" units="%"/></td>
                 </tr>
             );
     }
@@ -20,9 +30,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th> City </th>
-                        <th> Temperature </th>
-                        <th> Pressure </th>
-                        <th> Humidity </th>
+                        <th> Temperature (K) </th>
+                        <th> Pressure (hPa)</th>
+                        <th> Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody> 
